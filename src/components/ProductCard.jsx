6673fca1c1c74849e8fdd10d442b17055e746b18
Den <103452827+LourdenB15@users.noreply.github.com>
2025-08-13 21:43:16 +0800

    Update product image URLs to use local assets

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,19 +1,19 @@
 import "../css/ProductCard.css";
 
-function ProductCard(props) {
+function ProductCard({Product}) {
   return (
     <div className="card">
         <div className="card-wrapper">
             <div className="card-right">
                 <div className="img-wrapper">
-                    <img src={props.Product.url} alt={props.Product.name} />
+                    <img src={Product.url} alt={Product.name} />
                 </div>
             </div>
             <div className="card-left">
                 <div className="info-wrapper">
-                    <div className="product-name">{props.Product.name}</div>
-                    <div className="product-description">{props.Product.description}</div>
-                    <div className="product-price">&#8369; {props.Product.price.toLocaleString()}</div>
+                    <div className="product-name">{Product.name}</div>
+                    <div className="product-description">{Product.description}</div>
+                    <div className="product-price">&#8369; {Product.price.toLocaleString()}</div>
                      <button>Buy Now</button>
                 </div>
             </div>
